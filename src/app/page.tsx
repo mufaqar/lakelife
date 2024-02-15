a040ac45cea6d14840109d67f5f4cef052335ef7
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2024-02-15 15:11:31 +0500

    CategoryBox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import CategorySec from '@/components/blog/category-sec';
 import BannerSlider from '@/components/home/banner-slider'
 import Tabs from '@/components/home/tabs'
+import CategoryBox from '@/components/products/categ-box';
 import { TabsData } from '@/const/tabs';
+import { ProductsData } from '@/const/products';
 
 export default function Home() {
   return (
@@ -11,7 +13,7 @@ export default function Home() {
       </section>
       <section className='py-16'>
         <div className='container mx-auto px-3'>
-          {TabsData?.slice(1,2).map((item, idx) => {
+          {TabsData?.slice(1, 2).map((item, idx) => {
             return (<CategorySec key={idx} data={item} />
             )
           })}
@@ -25,6 +27,14 @@ export default function Home() {
           <div>
             <Tabs />
           </div>
+        </div>
+      </section>
+      <section className='py-16'>
+        <div className='container mx-auto px-3 grid md:grid-cols-3 grid-cols-1 gap-8'>
+          {ProductsData?.map((item, idx) => {
+            return (<CategoryBox key={idx} data={item} />
+            )
+          })}
         </div>
       </section>
     </main>
---
+++ src/const/products.jsx
@@ -0,0 +1,20 @@
+export const ProductsData = [
+    {
+        id: 1,
+        categ: 'Boots',
+               link: "#",
+        img: "/assets/images/product/1.png",
+    },
+    {
+        id: 2,
+        categ: 'Apparel',
+               link: "#",
+        img: "/assets/images/product/2.jpg",
+    },
+    {
+        id: 3,
+        categ: 'Accessories',
+               link: "#",
+        img: "/assets/images/product/3.png",
+    },
+]
